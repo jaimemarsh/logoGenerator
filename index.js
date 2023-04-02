ec15d8be1f26d57976a0565878aa4c610956b757
Jaime M <jaimemarsh19@gmail.com>
2023-04-02 14:50:51 -0400

    errors, something wrong with node

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
 
+const files = require('fs');
+const { writeFile } = require('fs/promises');
+const SVG = require('./shapes.js');
+const { Cricle, Square, Triangle } = require('./shapes.js');
+
+class Svg {
+    constructor() {
+        this.textElement = "";
+        this.shapeElement = "";
+    }
+    render() {
+        return `svg version="1.1" xmlns="http://www.w3.org/200/svg" width="300" height="200" viewBox="0 0 `
+    }
+    setTextElement(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="30" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape, color) {
+        this.shapeElement = shape.render()
+    }
+}
 
 //questions for user input
+
 const questions = [
     {
         type: "input",
         name: "text",
-        message: "Enter up to three characters:"
+        message: "Enter up to three characters:",
+        validate: (textChoice) =>
+            textChoice.length <= 3 || "Text must be less than 3 characters"
     },
     {
         type: "input",
